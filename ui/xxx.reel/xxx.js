9a3342331cae5ac318004e08f7d59a61d09a01da
jawdatls <noreply>
2018-01-09 11:45:38 +0000

    Add component xxx

---
+++ ui/xxx.reel/xxx.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/xxx.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Xxx
+ * @extends Component
+ */
+exports.Xxx = Component.specialize(/** @lends Xxx# */ {
+    constructor: {
+        value: function Xxx() {
+            this.super();
+        }
+    }
+});
