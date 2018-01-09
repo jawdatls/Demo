ef3697c0599ccd5f54ab6b0c56605487eaeae4df
jawdatls <noreply>
2018-01-09 11:20:56 +0000

    Add component xom

---
+++ ui/xom.reel/xom.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/xom.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Xom
+ * @extends Component
+ */
+exports.Xom = Component.specialize(/** @lends Xom# */ {
+    constructor: {
+        value: function Xom() {
+            this.super();
+        }
+    }
+});
